1a3d758ab1660aa84f198f91dfae798be5ea34cf
Obiora Odimmegwa <48391689+ObioraOdimmegwa@users.noreply.github.com>
2022-09-19 18:12:03 +0100

    Update video.js

--- video.js
+++ video.js
@@ -8,7 +8,7 @@ function count() {
   setTimeout(function () {
    sessionStorage.setItem("current", "true");
     window.location.replace("https://malkain.com/");
-  }, 11000);
+  }, 7000);
 }
 
 count();
